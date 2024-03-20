381666f6d2d2d82f73ee69f0a5fda57714f74112
Marko Cvijetic <marko.cvijetic@vega.local>
2024-03-20 14:05:08 +0100

    Fix problem with frontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'selling-used-items-app-frontend';
+}
