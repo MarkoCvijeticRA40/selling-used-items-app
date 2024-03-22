d7b22a378ff7745ca72e9af619992eb0c0db5953
Marko Cvijetic <marko.cvijetic@vega.local>
2024-03-22 12:11:53 +0100

    Fix problems with register page

--- selling-used-items-app-frontend/src/app/register/register.component.ts
+++ selling-used-items-app-frontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  ngOnInit() {
+  }
+
+  constructor(private router: Router) { }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
